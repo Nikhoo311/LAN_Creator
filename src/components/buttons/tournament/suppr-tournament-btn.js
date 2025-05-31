e520607b3779e948b232b917c5eaf049e093fe70
Nikho311 <nikho311.dev@gmail.com>
2025-05-31 20:06:39 +0200

    feat: add interaction for delete tournament

---
+++ src/components/buttons/tournament/suppr-tournament-btn.js
@@ -0,0 +1,91 @@
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, MessageFlags, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require("discord.js");
+const { color } = require("../../../../config/config.json");
+
+module.exports = {
+    data: {
+        name: "suppr-tournament-btn"
+    },
+    async execute(interaction, client) {
+        const { tournaments } = client;
+        if (tournaments.size == 0) {
+            return interaction.reply({ content: "❌ Je ne dispose d'aucun Tournois... Pour avoir accès à cette partie, il faut créer un Tournois et toutes les informaitons y seront afficher.", flags: [MessageFlags.Ephemeral] })
+        }
+        const serveralTournaments = tournaments.size > 1;
+        const currentTournament = !serveralTournaments ? tournaments.first() : tournaments;
+
+        let message = `# Espace de suppression de Tournois`;
+        if (serveralTournaments) {
+            message += "\nMerci de choisir le Tournois qui a besoin d'être supprimer";
+            let selectTournament = new StringSelectMenuBuilder()
+                .setCustomId("select-suppr-tournament")
+                .setMinValues(1)
+                .setMaxValues(1)
+                .setPlaceholder("Choisir un Tournois")
+                        
+            tournaments.forEach(tournament => {
+                selectTournament.addOptions(
+                    new StringSelectMenuOptionBuilder()
+                    .setEmoji("🎮")
+                    .setLabel(tournament.name)
+                    .setValue(tournament.id)
+                )
+            });
+            return interaction.reply({ content: message, components: [new ActionRowBuilder().addComponents(selectTournament)], flags: [MessageFlags.Ephemeral] })
+        } else {
+            message += ` pour \`\`${currentTournament.name}\`\`\nCet espace est dédié à la gestion du Tournois !\n# Informations :\n`;
+            const gamePossible = JSON.parse(readFileSync('./config/bd.json', 'utf-8'))["tournamentGames"];
+            const gameChosen = gamePossible.filter(game => generateSlug(currentTournament.game) == generateSlug(game.name))[0];
+            
+            if (!gameChosen) {
+                return interaction.reply({ content: "❌ Aucun jeu n'a été trouver... Recommencez"})
+            }
+            
+            const embedStats = new EmbedBuilder()
+                .setColor(color.green)
+                .setDescription(`# Score : ${currentTournament.scoreA} - ${currentTournament.scoreB}`)
+                .addFields(
+                    { name: "**Jeu**", value: `> ${gameChosen.emoji} ${gameChosen.name}`, inline: true },
+                    { name: "**Nombre de match(s)**", value: `> ${currentTournament.matches.length}`, inline: false },
+                )
+            let i = 0;
+            const guild = interaction.guild;
+            currentTournament.teams.forEach(team => {
+                let teamDisplay = "";
+                team.players.forEach(player => {
+                    teamDisplay += `- <@${guild.members.cache.find(m => player.name == m.displayName).user.id}>\n`
+
+                })
+                embedStats.addFields({
+                    name: `${i >= 1 ? "\u200B" : "**Équipes**"}`, value: `> ${team.name}\n${teamDisplay}`, inline: true
+                })
+                i++;
+            })
+
+            const matchBtn = new ButtonBuilder()
+                .setCustomId("match-btn")
+                .setStyle(ButtonStyle.Primary)
+                .setLabel("Créer un Match")
+                .setEmoji("🎮")
+            
+            const managementMatchBtn = new ButtonBuilder()
+                .setCustomId("management-match-btn")
+                .setStyle(ButtonStyle.Secondary)
+                .setLabel("Gestion des Matchs")
+                .setEmoji("⚙️")
+            
+            const supprMatchBtn = new ButtonBuilder()
+                .setCustomId("suppr-match-btn")
+                .setStyle(ButtonStyle.Danger)
+                .setLabel("Supprimer un Match")
+                .setEmoji('✖️')
+            
+            const createVocalsChannelsBtn = new ButtonBuilder()
+                .setCustomId("create-vocals-channels-btn")
+                .setStyle(ButtonStyle.Success)
+                .setLabel("Créer les salons vocaux d'équipes")
+                .setEmoji('🔊')
+            
+            interaction.reply({ content: message, embeds: [embedStats], components: [new ActionRowBuilder().addComponents(matchBtn).addComponents(managementMatchBtn).addComponents(supprMatchBtn), new ActionRowBuilder().addComponents(createVocalsChannelsBtn)] });
+        }
+    }
+}
--- src/components/selectMenus/tournament/select-tournament.js
+++ src/components/selectMenus/tournament/select-tournament.js
@@ -6,14 +6,14 @@ const { generateSlug } = require('../../../functions/utils/generateSlug');
 
 module.exports = {
     data: {
-        name: "select-tournament"
+        name: "select-tournament",
+        multi: "select-suppr-tournament"
     },
     async execute(interaction, client) {
         const info = interaction.values[0];
         const { tournaments } = client;
-
         const tournament = tournaments.get(info);
-        let message = `# Espace de gestion de Tournois pour \`\`${tournament.name}\`\`\nCet espace est dédié à la gestion du Tournois !\n# Informations :\n`;
+
         const gamePossible = JSON.parse(readFileSync('./config/bd.json', 'utf-8'))["tournamentGames"];
         const gameChosen = gamePossible.filter(game => generateSlug(tournament.game) == generateSlug(game.name))[0];
         
@@ -42,30 +42,51 @@ module.exports = {
             i++;
         })
 
-        const matchBtn = new ButtonBuilder()
-            .setCustomId("match-btn")
-            .setStyle(ButtonStyle.Primary)
-            .setLabel("Créer un Match")
-            .setEmoji("🎮")
-        
-        const managementMatchBtn = new ButtonBuilder()
-            .setCustomId("management-match-btn")
-            .setStyle(ButtonStyle.Secondary)
-            .setLabel("Gestion des Matchs")
-            .setEmoji("⚙️")
-        
-        const supprMatchBtn = new ButtonBuilder()
-            .setCustomId("suppr-match-btn")
-            .setStyle(ButtonStyle.Danger)
-            .setLabel("Supprimer un Match")
-            .setEmoji('✖️')
-        
-        const createVocalsChannelsBtn = new ButtonBuilder()
-            .setCustomId("create-vocals-channels-btn")
-            .setStyle(ButtonStyle.Success)
-            .setLabel("Créer les salons vocaux d'équipes")
-            .setEmoji('🔊')
-        
-        interaction.update({ content: message, embeds: [embedStats], components: [new ActionRowBuilder().addComponents(matchBtn).addComponents(managementMatchBtn).addComponents(supprMatchBtn), new ActionRowBuilder().addComponents(createVocalsChannelsBtn)] });
+        let message = "# Espace de ";
+        if (interaction.customId == "select-suppr-tournament") {
+            message += `suppression du tournois \`\`${tournament.name}\`\`\n-# ID: \`\`${tournament.id}\`\`\n ## Informations :\n Pour supprimer ce Tounois il suffit de cliquer sur le bouton \`\`Oui\`\`.\n\n\`\`\`\n\n Es-tu sûr de bien vouloir supprimer ${tournament.name} ?\n\`\`\``;
+            const supprYesTournament = new ButtonBuilder()
+                .setCustomId("suppr-yes-btn")
+                .setEmoji('✅')
+                .setLabel("Oui")
+                .setStyle(ButtonStyle.Success)
+            
+            const supprNoTournament = new ButtonBuilder()
+                .setCustomId("suppr-no-btn")
+                .setEmoji('✖️')
+                .setLabel("Non")
+                .setStyle(ButtonStyle.Danger)
+                
+            interaction.update({ content: message, embeds:[embedStats], components: [new ActionRowBuilder().addComponents([supprYesTournament, supprNoTournament])] });
+
+        } else {
+            message += `gestion de Tournois pour \`\`${tournament.name}\`\`\nCet espace est dédié à la gestion du Tournois !\n## Informations :\n`;
+    
+            const matchBtn = new ButtonBuilder()
+                .setCustomId("match-btn")
+                .setStyle(ButtonStyle.Primary)
+                .setLabel("Créer un Match")
+                .setEmoji("🎮")
+            
+            const managementMatchBtn = new ButtonBuilder()
+                .setCustomId("management-match-btn")
+                .setStyle(ButtonStyle.Secondary)
+                .setLabel("Gestion des Matchs")
+                .setEmoji("⚙️")
+            
+            const supprMatchBtn = new ButtonBuilder()
+                .setCustomId("suppr-match-btn")
+                .setStyle(ButtonStyle.Danger)
+                .setLabel("Supprimer un Match")
+                .setEmoji('✖️')
+            
+            const createVocalsChannelsBtn = new ButtonBuilder()
+                .setCustomId("create-vocals-channels-btn")
+                .setStyle(ButtonStyle.Success)
+                .setLabel("Créer les salons vocaux d'équipes")
+                .setEmoji('🔊')
+            
+            interaction.update({ content: message, embeds: [embedStats], components: [new ActionRowBuilder().addComponents(matchBtn).addComponents(managementMatchBtn).addComponents(supprMatchBtn), new ActionRowBuilder().addComponents(createVocalsChannelsBtn)] });
+        }
     }
 }
