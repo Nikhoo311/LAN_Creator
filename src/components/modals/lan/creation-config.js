fff0c2553ebd4a3010ec09dd2ee3dec37ba5cd17
Nikho311 <nikho311.dev@gmail.com>
2025-10-03 20:11:08 +0200

    fix: useless things

--- src/components/modals/lan/creation-config.js
+++ src/components/modals/lan/creation-config.js
@@ -12,38 +12,33 @@ module.exports = {
         const configHours = interaction.fields.getTextInputValue("config_hours");
         const configMaterials = interaction.fields.getTextInputValue("config_material") || null;
         
-        const file = JSON.parse(readFileSync("./config/bd.json", "utf-8"));
-        
-        function getInfoConfig(name) {
-            let result;
-            file["bd"].forEach(element => {
-                if (element.name == name) {
-                    result = element
-                }
-            });
-            return result;
-        }
-
-        if (getInfoConfig(configName)) {
-            return interaction.reply({ content: `❌ Une configuration au nom de \`${configName}\` existe déjà...`, flags: [MessageFlags.Ephemeral] })
-        }
-
-        file[`bd`].push({
-            name: configName, adress: configAdress, hours: configHours, materials: configMaterials
-        })
-        // Write in the data base
         try {
+            const alreadyExist = await Config.findOne({ name: configName });
+
+            if (alreadyExist) {
+                return interaction.reply({ 
+                    content: `❌ Une configuration au nom de \`${configName}\` existe déjà...`, 
+                    flags: [MessageFlags.Ephemeral] 
+                });
+            }
             await Config.create({
                 name: configName,
                 adress: configAdress,
                 hours: configHours,
                 materials: configMaterials ?? "Aucun",      
-            })
-            interaction.reply({ content: `✅ La configuration \`${configName}\` à bien été créer avec succès !`, flags: [MessageFlags.Ephemeral]})
+            });
+
+            interaction.reply({ 
+                content: `✅ La configuration \`${configName}\` a bien été créée avec succès !`, 
+                flags: [MessageFlags.Ephemeral]
+            });
             
         } catch (error) {
-            interaction.reply({ content: "❌ Un erreur est survenu lors de l'écriture dans le fichier de configuration !", flags: [MessageFlags.Ephemeral] })
+            console.error(error);
+            interaction.reply({ 
+                content: "❌ Une erreur est survenue lors de l'écriture dans la configuration !", 
+                flags: [MessageFlags.Ephemeral] 
+            });
         }
-
     }
 }
