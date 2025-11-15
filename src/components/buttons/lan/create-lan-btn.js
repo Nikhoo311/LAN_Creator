950c9ba31f875c73b93e35f72b8af6680c845d5c
Nikho311 <nikho311.dev@gmail.com>
2025-11-15 23:03:16 +0100

    fix: add LabelComponent in modals

--- src/components/buttons/lan/create-lan-btn.js
+++ src/components/buttons/lan/create-lan-btn.js
@@ -1,35 +1,30 @@
-const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, LabelBuilder } = require("discord.js");
+const { ModalBuilder, TextInputBuilder, TextInputStyle, LabelBuilder } = require("discord.js");
 module.exports = {
     data: {
         name: "create-lan-btn"
     },
-    async execute (interaction, client) {
-        
+    async execute (interaction, client) {        
         const modal = new ModalBuilder()
-        .setCustomId("lan_create")
-        .setTitle("Création LAN")
-
-        const textInputLabel = new LabelBuilder()
-            .setTextInputComponent(textInput)
-            .setLabel("Quel est le nom de LAN ?")
-        
+            .setCustomId("lan_create")
+            .setTitle("Création LAN")
+ 
         const textInput = new TextInputBuilder()
             .setCustomId("lan_name")
             .setRequired(true)
             .setStyle(TextInputStyle.Short);
         
-        const textGoogleSheetLabel = new LabelBuilder()
-            .setTextInputComponent(textGoogleSheet)
-            .setLabel("Quel est le Google Sheet de la LAN ?")
+        const textInputLabel = new LabelBuilder()
+            .setTextInputComponent(textInput)
+            .setLabel("Quel est le nom de LAN ?")
 
         const textGoogleSheet = new TextInputBuilder()
             .setCustomId("lan_google_sheet")
             .setRequired(false)
             .setStyle(TextInputStyle.Short);
 
-        const textNbVocLabel = new LabelBuilder()   
-            .setTextInputComponent(textNbVoc)
-            .setLabel("Quel est le nombre de salons vocaux ?")
+        const textGoogleSheetLabel = new LabelBuilder()
+            .setTextInputComponent(textGoogleSheet)
+            .setLabel("Quel est le Google Sheet de la LAN ?")
         
         const textNbVoc = new TextInputBuilder()
             .setCustomId("lan_nb_voc")
@@ -37,7 +32,11 @@ module.exports = {
             .setPlaceholder("Entre 1 et 5 | Par défaut : 1")
             .setStyle(TextInputStyle.Short);
 
-        modal.addComponents([new ActionRowBuilder().addComponents(textInputLabel), new ActionRowBuilder().addComponents(textGoogleSheetLabel), new ActionRowBuilder().addComponents(textNbVocLabel)])
-        await interaction.showModal(modal)
+        const textNbVocLabel = new LabelBuilder()   
+            .setTextInputComponent(textNbVoc)
+            .setLabel("Quel est le nombre de salons vocaux ?")
+
+        modal.addLabelComponents(textInputLabel, textGoogleSheetLabel, textNbVocLabel)
+        return await interaction.showModal(modal)
     }
 }
--- src/components/buttons/lan/creation-config-btn.js
+++ src/components/buttons/lan/creation-config-btn.js
@@ -1,43 +1,53 @@
-const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
+const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, LabelBuilder } = require("discord.js");
 module.exports = {
     data: {
         name: "creation-config-btn"
     },
     async execute (interaction, client) {
-        
         const modal = new ModalBuilder()
             .setCustomId("creation-config")
             .setTitle("Création d'une nouvelle configuration de LAN")
 
         const textInput = new TextInputBuilder()
             .setCustomId("config_name")
-            .setLabel("Quel est le nom de la configuration de LAN ?")
             .setRequired(true)
             .setStyle(TextInputStyle.Short);
-        
+
+        const textInputLabel = new LabelBuilder()
+            .setTextInputComponent(textInput)
+            .setLabel("Quel est le nom de la configuration de LAN ?")
+
         const textaddress = new TextInputBuilder()
             .setCustomId("config_address")
-            .setLabel("Quelle est l'addresse de la LAN ?")
             .setRequired(true)
             .setStyle(TextInputStyle.Short)
             .setPlaceholder("999 rue des champignons braisé - 05125 La Forêt")
 
+        const textAddressLabel = new LabelBuilder()
+            .setTextInputComponent(textaddress)
+            .setLabel("Quelle est l'addresse de la LAN ?")
+
         const textHours = new TextInputBuilder()
             .setCustomId("config_hours")
-            .setLabel("Quelle est l'heure d'arrivé de la LAN ?")
             .setRequired(true)
             .setStyle(TextInputStyle.Short)
             .setPlaceholder("19h00")
+    
+        const textHoursLabel = new LabelBuilder()
+            .setTextInputComponent(textHours)
+            .setLabel("Quelle est l'heure d'arrivé de la LAN ?")
 
         const textMaterial = new TextInputBuilder()
             .setCustomId("config_material")
-            .setLabel("Quel est le matériel disponible pour la LAN ?")
             .setRequired(false)
             .setPlaceholder("Par défaut : Aucun")
             .setStyle(TextInputStyle.Paragraph)
             .setMaxLength(1500);
-
-        modal.addComponents([new ActionRowBuilder().addComponents(textInput), new ActionRowBuilder().addComponents(textaddress), new ActionRowBuilder().addComponents(textHours), new ActionRowBuilder().addComponents(textMaterial)])
-        await interaction.showModal(modal)
+            
+        const textMaterialLabel = new LabelBuilder()
+            .setLabel("Quel est le matériel disponible pour la LAN ?")
+            .setTextInputComponent(textMaterial)
+        modal.addLabelComponents(textInputLabel, textAddressLabel, textHoursLabel, textMaterialLabel)
+        return await interaction.showModal(modal)
     }
 }
