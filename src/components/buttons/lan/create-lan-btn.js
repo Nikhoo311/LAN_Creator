ab35bc94f0cb170f545c2dc3a6f6d7fb9e7ecf50
Nikho311 <nikho311.dev@gmail.com>
2025-11-15 22:44:18 +0100

    fix: create lan -> add LabelComponent in modal

--- src/components/buttons/lan/create-lan-btn.js
+++ src/components/buttons/lan/create-lan-btn.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
+const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, LabelBuilder } = require("discord.js");
 module.exports = {
     data: {
         name: "create-lan-btn"
@@ -9,26 +9,35 @@ module.exports = {
         .setCustomId("lan_create")
         .setTitle("Création LAN")
 
+        const textInputLabel = new LabelBuilder()
+            .setTextInputComponent(textInput)
+            .setLabel("Quel est le nom de LAN ?")
+        
         const textInput = new TextInputBuilder()
             .setCustomId("lan_name")
-            .setLabel("Quel est le nom de LAN ?")
             .setRequired(true)
             .setStyle(TextInputStyle.Short);
         
+        const textGoogleSheetLabel = new LabelBuilder()
+            .setTextInputComponent(textGoogleSheet)
+            .setLabel("Quel est le Google Sheet de la LAN ?")
+
         const textGoogleSheet = new TextInputBuilder()
             .setCustomId("lan_google_sheet")
-            .setLabel("Quel est le Google Sheet de la LAN ?")
             .setRequired(false)
             .setStyle(TextInputStyle.Short);
 
+        const textNbVocLabel = new LabelBuilder()   
+            .setTextInputComponent(textNbVoc)
+            .setLabel("Quel est le nombre de salons vocaux ?")
+        
         const textNbVoc = new TextInputBuilder()
             .setCustomId("lan_nb_voc")
-            .setLabel("Quel est le nombre de salons vocaux ?")
             .setRequired(false)
             .setPlaceholder("Entre 1 et 5 | Par défaut : 1")
             .setStyle(TextInputStyle.Short);
 
-        modal.addComponents([new ActionRowBuilder().addComponents(textInput), new ActionRowBuilder().addComponents(textGoogleSheet), new ActionRowBuilder().addComponents(textNbVoc)])
+        modal.addComponents([new ActionRowBuilder().addComponents(textInputLabel), new ActionRowBuilder().addComponents(textGoogleSheetLabel), new ActionRowBuilder().addComponents(textNbVocLabel)])
         await interaction.showModal(modal)
     }
 }
