ed73e91b9c80f8b1f94fa734c3fb9e016993a9a3
Nikho311 <nikho311.dev@gmail.com>
2026-05-27 22:50:45 +0200

    style(container): add container with options

--- src/components/buttons/lan/lan-management-btn.js
+++ src/components/buttons/lan/lan-management-btn.js
@@ -1,4 +1,4 @@
-const { MessageFlags, ContainerBuilder } = require('discord.js');
+const { MessageFlags, ContainerBuilder, TextDisplayBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, SeparatorBuilder, SeparatorSpacingSize } = require('discord.js');
 const { Lan } = require('../../../class/Lan');
 const { getLanForGuild } = require("../../../functions/utils/guildCache");
 const logger = require("../../../functions/utils/Logger");
@@ -9,7 +9,9 @@ module.exports = {
         name: "lan-management-btn"
     },
     async execute (interaction, client) {
-        const generalChannel = interaction.guild.channels.cache.get(interaction.channelId)?.parent
+        await interaction.deferUpdate();
+
+        const generalChannel = interaction.guild.channels.cache.get(interaction.channelId)?.parent;
         const lanId = generalChannel?.topic;
 
         const lan = getLanForGuild(client, lanId, interaction.guildId);
@@ -17,7 +19,45 @@ module.exports = {
             return interaction.reply({ content: "❌ LAN introuvable sur ce serveur.", flags: [MessageFlags.Ephemeral] });
         }
 
+        const text = new TextDisplayBuilder({ content: `Voici la liste des options possibles pour gérer la LAN :\n* Modifier la liste des participants\n* Verrouiller l'acces à la LAN à certains membres du serveur (${interaction.guild.name})\n* Modifier le flyer\n* Supprimer la LAN\n\n-# La liste des options n'est pas définitive` });
+
+        const deleteLanButton = new ButtonBuilder()
+            .setCustomId("delete-lan-btn")
+            .setLabel("Supprimer la LAN")
+            .setStyle(ButtonStyle.Danger)
+            .setEmoji("<:trash:1378419101751447582>")
+
+        const editParticipantsButton = new ButtonBuilder()
+            .setCustomId("edit-participants-btn")
+            .setLabel("Modifier la liste des participants")
+            .setStyle(ButtonStyle.Secondary)
+            .setEmoji("📝")
+        
+        const editPermissionsButton = new ButtonBuilder()
+            .setCustomId("edit-permissions-btn")
+            .setLabel("Modifier les permissions")
+            .setStyle(ButtonStyle.Secondary)
+            .setEmoji("🔒")
+        
+        const editFlyerButton = new ButtonBuilder()
+            .setCustomId("edit-flyer-btn")
+            .setLabel("Modifier le flyer")
+            .setStyle(ButtonStyle.Secondary)
+            .setEmoji("🖼️")
+
+        const actionsRowOne = new ActionRowBuilder()
+            .addComponents(editParticipantsButton, editPermissionsButton, editFlyerButton);
+        
+        const actionsRowTwo = new ActionRowBuilder()
+            .addComponents(deleteLanButton);
+
+        const separator = new SeparatorBuilder().setDivider(true).setSpacing(SeparatorSpacingSize.Small)
         const container = new ContainerBuilder()
-            
+            .addTextDisplayComponents(text)
+            .addSeparatorComponents(separator)
+            .addActionRowComponents(actionsRowOne)
+            .addActionRowComponents(actionsRowTwo)
+        
+        return interaction.channel.send({ components: [container], flags: [MessageFlags.IsComponentsV2] });
     }
 }
